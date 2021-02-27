6baf20c56a163b4dacea3a644e4c7186ec0f0faa
abhi-kv29 <keshav.shm@gmail.com>
2021-02-27 07:48:54 +0530

    added actual calling and mailing options

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-void main(){
+void main() {
   return runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -13,16 +14,15 @@ void main(){
           flexibleSpace: Container(
             decoration: BoxDecoration(
               gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: <Color>[
-                  Colors.blueAccent.shade200,
-                  Colors.deepPurpleAccent.shade100,
-                ]
-              ),
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: <Color>[
+                    Colors.blueAccent.shade200,
+                    Colors.deepPurpleAccent.shade100,
+                  ]),
             ),
           ),
-      ),
+        ),
       ),
     ),
   );
@@ -38,19 +38,19 @@ class _State extends State<HomePage> {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-            Container(
-              padding: EdgeInsets.fromLTRB(0.0, 90.0, 5.0, 0),
-              margin: EdgeInsets.all(20.0),
-              alignment: Alignment(0.0, 0.0),
-              child: CircleAvatar(
-              radius: 80,
-              backgroundImage: AssetImage('images/kv1.jpg'),
-            ),
-            ),
         Container(
-
+          padding: EdgeInsets.fromLTRB(0.0, 90.0, 5.0, 0),
+          margin: EdgeInsets.all(20.0),
           alignment: Alignment(0.0, 0.0),
-          child: Text('Abhishek Sharma',
+          child: CircleAvatar(
+            radius: 80,
+            backgroundImage: AssetImage('images/kv1.jpg'),
+          ),
+        ),
+        Container(
+          alignment: Alignment(0.0, 0.0),
+          child: Text(
+            'Abhishek Sharma',
             style: TextStyle(
               letterSpacing: 2,
               fontFamily: 'Lobster',
@@ -60,58 +60,65 @@ class _State extends State<HomePage> {
             ),
           ),
         ),
-            Container(
-              alignment: Alignment(0.0, 0.0),
-              margin: EdgeInsets.all(20.0),
-            child: Text(
-              'FLUTTER DEVELOPER',
-              style: TextStyle(
-                letterSpacing: 7,
-                color: Colors.blue.shade50,
-                fontFamily: 'Source Sans Pro',
-                fontSize: 17,
-                fontWeight: FontWeight.bold,
-              ),
+        Container(
+          alignment: Alignment(0.0, 0.0),
+          margin: EdgeInsets.all(20.0),
+          child: Text(
+            'WEB/FLUTTER DEVELOPER',
+            style: TextStyle(
+              letterSpacing: 7,
+              color: Colors.blue.shade50,
+              fontFamily: 'Source Sans Pro',
+              fontSize: 17,
+              fontWeight: FontWeight.bold,
             ),
-            ),
-          SizedBox(
-            height: 5,
-            width: 150,
-            child: Divider(
-                color: Colors.blue.shade900,
-            ),
-          ),
-        Card(
-          color: Colors.white,
-          margin: EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
-          child: Padding(
-            padding: EdgeInsets.all(10.0),
-          child: Row(
-            children: <Widget>[
-              Icon(
-                Icons.phone,
-                color: Colors.blue.shade400,
-              ),
-              SizedBox(
-                width: 15,
-              ),
-              Text(
-                '+91 88005 45528',
-                style: TextStyle(
-                  color: Colors.blue.shade400,
-                  fontFamily: 'Source Sans Pro',
-                  fontSize: 20,
-                ),
-              ),
-            ],
-          ),
           ),
         ),
-        Card(
-          color: Colors.white,
-          margin: EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
-          child: Padding(
-            padding: EdgeInsets.all(10.0),
+        SizedBox(
+          height: 30,
+          width: 150,
+          child: Divider(
+            color: Colors.blue.shade900,
+          ),
+        ),
+        SizedBox(
+          height: 50.0,
+          width: 300.0,
+          child: RaisedButton(
+            onPressed: () => launch("tel://8826014829"),
+            color: Colors.white,
+            child: Row(
+              children: <Widget>[
+                Icon(
+                  Icons.phone,
+                  color: Colors.blue.shade400,
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                Center(
+                  child: Text(
+                    'CALL',
+                    style: TextStyle(
+                      color: Colors.blue.shade400,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 30.0,
+        ),
+        SizedBox(
+          height: 50.0,
+          width: 300.0,
+          child: RaisedButton(
+            color: Colors.white,
+            onPressed: () => launch("mailto:keshav.shm@gmail.com"),
             child: Row(
               children: <Widget>[
                 Icon(
@@ -122,7 +129,7 @@ class _State extends State<HomePage> {
                   width: 15,
                 ),
                 Text(
-                  'keshav.shm@gmail.com',
+                  'MAIL',
                   style: TextStyle(
                     color: Colors.blue.shade400,
                     fontFamily: 'Source Sans Pro',
@@ -133,15 +140,7 @@ class _State extends State<HomePage> {
             ),
           ),
         ),
-        ],
-
+      ],
     );
-
   }
 }
-
-
-
-
-
-
